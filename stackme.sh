c2883689a6e6843c581d56001b6c74aacd34f8bb
German Eichberger <German.eichberger@rackspace.com>
2017-08-30 09:50:31 -0700

    Adapt for FWaaS V2

--- stackme.sh
+++ stackme.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-OCTAVIA_PATCH=""
-OCTAVIA_CLIENT_PATCH=""
-BARBICAN_PATCH=""
+FWAAS_PATCH=""
+FWAAS_DASHBOARD_PATCH=""
 
 # Quick sanity check (should be run on Ubuntu 16.04 and MUST be run as root directly)
 if [ `lsb_release -rs` != "16.04" ]
@@ -23,7 +22,7 @@ apt-get install git vim jq -y
 # Clone the devstack repo
 git clone https://github.com/openstack-dev/devstack.git /tmp/devstack
 
-wget -O - https://raw.githubusercontent.com/rm-you/devstack_deploy/master/local.conf > /tmp/devstack/local.conf
+wget -O - https://raw.githubusercontent.com/xgerman/devstack_deploy/master/local.conf > /tmp/devstack/local.conf
 
 # Create the stack user
 /tmp/devstack/tools/create-stack-user.sh
@@ -37,10 +36,8 @@ chown -R stack:stack /opt/stack/devstack/
 
 cat >>/opt/stack/.profile <<EOF
 # Prepare patches for local.conf
-export BARBICAN_PATCH="$BARBICAN_PATCH"
-export OCTAVIA_PATCH="$OCTAVIA_PATCH"
-export OCTAVIACLIENT_BRANCH="$OCTAVIA_CLIENT_PATCH"
-export OCTAVIA_AMP_BASE_OS="centos"
+export FWAAS_PATCH="$FWAAS_PATCH"
+export FWAAS_DASHBOARD_PATCH="$FWAAS_DASHBOARD_PATCH"
 EOF
 
 # Precreate .cache so it won't have the wrong perms
@@ -56,17 +53,7 @@ route del default gw 192.168.0.1 &> /dev/null
 # Install tox globally
 pip install tox &> /dev/null
 
-# Grab utility scripts from github and add them to stack's .profile
-wget -q -O - https://raw.githubusercontent.com/rm-you/devstack_deploy/master/profile | sudo -u stack tee -a /opt/stack/.bash_profile > /dev/null
-
-# Set up barbican container
-sudo -u stack wget -q https://raw.githubusercontent.com/rm-you/devstack_deploy/master/make_container.sh -O /opt/stack/make_container.sh
-chmod +x /opt/stack/make_container.sh
-sudo su - stack -c /opt/stack/make_container.sh
-
-# Fix missing route
-ROUTER_IP=$(su - stack -c "openstack router show router1 | awk -F '|' ' / external_gateway_info / {print \$3} ' | jq -r '.external_fixed_ips[0].ip_address'")
-route add -net 10.0.0.0 netmask 255.255.255.0 gw $ROUTER_IP dev br-ex
+# Install Dashboard
 
 # Drop into a shell
 su - stack
